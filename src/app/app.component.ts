334ddd766afa7406d6b962710cbcc5adad183833
Naresh Kumar H <nareshkumarh@live.com>
2019-07-13 23:14:27 -0400

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-structural-directives';
+
+  name = "naresh";
+  displayName = false;
+  color = "red";
+  colors = ["red","blue","green"]
+  items = [];
 }
